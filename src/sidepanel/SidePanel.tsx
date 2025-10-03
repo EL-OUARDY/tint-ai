25147d642dd7f79c3bfe242130bef1865daeed9e
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-10-03 13:17:06 +0100

    Add shadcn UI components

--- src/sidepanel/SidePanel.tsx
+++ src/sidepanel/SidePanel.tsx
@@ -1,39 +1,8 @@
 import { Button } from '@/components/ui/button'
-import { useEffect } from 'react'
 
 export const SidePanel = () => {
-  useEffect(() => {
-    // Notify background that side panel is opened
-    chrome.runtime.sendMessage({ type: 'SIDEPANEL_OPENED' }).catch(() => {
-      console.log('Could not send message to background')
-    })
-
-    // Listen for close message from background
-    const handleMessage = (message: any) => {
-      if (message.type === 'CLOSE_SIDEPANEL') {
-        window.close()
-      }
-    }
-
-    chrome.runtime.onMessage.addListener(handleMessage)
-
-    // Notify background when side panel is being closed
-    const handleBeforeUnload = () => {
-      chrome.runtime.sendMessage({ type: 'SIDEPANEL_CLOSED' }).catch(() => {
-        // Ignore errors
-      })
-    }
-
-    window.addEventListener('beforeunload', handleBeforeUnload)
-
-    return () => {
-      chrome.runtime.onMessage.removeListener(handleMessage)
-      window.removeEventListener('beforeunload', handleBeforeUnload)
-    }
-  }, [])
-
   return (
-    <main>
+    <main className="bg-background p-8">
       <h3 className="text-2xl text-red-500">SidePanel Page</h3>
       <Button variant={'outline'}>Hello</Button>
     </main>
--- src/sidepanel/index.tsx
+++ src/sidepanel/index.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { SidePanel } from './SidePanel'
 import './index.css'
+import { ThemeProvider } from '@/contexts/theme-provider'
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
-    <SidePanel />
+    <ThemeProvider defaultTheme="system" storageKey="tintai-ui-theme">
+      <SidePanel />
+    </ThemeProvider>
   </React.StrictMode>,
 )
